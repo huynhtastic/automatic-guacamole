e79206dced377c7a7b4ba0b25823178947177e7a
Richard L Huynh <huynhtastic@gmail.com>
2018-11-08 16:23:07 -0600

    using separate component and image tag

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
 
 class Greeting extends Component {
-  render() {
-    return (
-      <Text>Hello, {this.props.name}</Text>
-    );
-  }
+	render() {
+		return (
+			<View style={{alignItems: 'center'}}>
+				<Text>Hello, {this.props.name}!</Text>
+			</View>
+		)
+	}
 }
 
 export default class App extends Component {
   render() {
+    let pic = 
+      { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' };
     return (
       <View style={styles.container}>
+        <Image source={pic} style={{width: 193, height: 110}} />
         <Greeting name='Kevin' />
       </View>
     );
